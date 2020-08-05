afaa5b0da88c9ba503fa185ba60c4dc1bd78c266
Andrea <a.g.baldan@gmail.com>
2020-08-06 01:20:16 +0200

    Update tests/test_app.py

--- pokespeare/app.py
+++ pokespeare/app.py
@@ -14,6 +14,7 @@ def get_pokemon_description(pokemon_name):
             f"https://pokeapi.co/api/v2/pokemon-species/{pokemon_name}"
         )
         response.raise_for_status()
+        print(response)
         pokemon = schema.load(response.json())
         shakespeare = requests.post(
             "https://api.funtranslations.com/translate/shakespeare.json",
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,33 @@
 import unittest
+from unittest.mock import patch
 from pokespeare.app import flask_app
+from pokespeare.exceptions import MalformedJSONResponseError
+
+
+expected_response = {
+    "name": "haunter",
+    "flavor_text_entries": [
+        {"language": {"name": "en"}, "flavor_text": "The best one."}
+    ],
+    "to_be_ignored": "ignore me",
+}
+
+wrong_response = {
+    "name": "haunter",
+    "wrong_field": [
+        {"language": {"name": "en"}, "flavor_text": "The best one."}
+    ],
+    "to_be_ignored": "ignore me",
+}
+
+
+class FakeResponse:
+    def __init__(self, status_code, expected_response):
+        self.status_code = status_code
+        self.expected_response = expected_response
+
+    def json(self):
+        return self.expected_response
 
 
 class AppTest(unittest.TestCase):
@@ -17,3 +45,11 @@ class AppTest(unittest.TestCase):
     def test_get_pokemon_description_wrong_method(self):
         result = self.app.post("/pokemon/haunter")
         self.assertEqual(result.status_code, 405)
+
+    @patch(
+        "pokespeare.app.requests.get",
+        return_value=FakeResponse(200, wrong_response),
+    )
+    def test_get_pokemon_description_wrong_field(self, requests_mock):
+        result = self.app.get("/pokemon/haunter")
+        self.assertTrue(result.status_code == 404)
