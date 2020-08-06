6a94c81ecd6ff6d9184a7fbd8a3b390878e9050b
Andrea <a.g.baldan@gmail.com>
2020-08-06 16:15:00 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="pokespeare",
     version="0.0.1",
@@ -7,8 +13,11 @@ setup(
     license="MIT",
     author="Andrea Giacomo Baldan",
     author_email="a.g.baldan@gmail.com",
-    description="",
+    description="Simple REST API to get a Pokemon description Shakespereanized",
+    long_description=readme,
     packages=find_packages(exclude=["tests"]),
+    install_requires=requirements,
+    tests_require=requirements,
     platforms="any",
     python_requires=">=3.7",
     test_suite="tests",
